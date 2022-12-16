eea71e912bf5a0ed46a55408ab7657d5f95ec4b9
izzaa0207 <izzathul.mardhiyah1385@student.unri.ac.id>
2022-12-16 18:44:45 +0700

    menambahkan satu project

---
+++ framework2/tugas-vuetify/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <img src="./assets/logo-unri2.png" height="75" width="550">
+      <web-toolbar>
+         <web-home>
+              <web-login></web-login>
+        </web-home>
+      </web-toolbar>
+          <router-view/>
+          <web-footer></web-footer>
+  </div>
+</template>
+
+<script>
+import WebFooter from './components/WebFooter.vue'
+import WebToolbar from './components/WebToolbar.vue'
+import WebLogin from './components/WebLogin.vue'
+import WebHome from './components/WebHome.vue'
+
+export default {
+  name: 'App',
+  components:{WebToolbar, WebHome, WebLogin, WebFooter}
+}
+</script>
+
+<style>
+</style>
