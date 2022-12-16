f9b67b0bd4dc15788d050514369b8e76cd72a9ec
izzaa0207 <izzathul.mardhiyah1385@student.unri.ac.id>
2022-12-16 18:08:50 +0700

    menambahkan project adventure game

---
+++ Adventure Game/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Adventure Game !");
+            Console.WriteLine("What's your name ?");
+            Novice player = new Novice();
+            player.Name = Console.ReadLine();
+            Console.WriteLine("Hi "+player.Name+", ready to begin the game ?[yes/no]");
+            string bReady = Console.ReadLine();
+            if(bReady=="yes"){
+                Console.WriteLine(player.Name+" is entering the world ...");
+                Enemy enemy1 = new Enemy("Chuuya");
+                Console.WriteLine(player.Name+" is encountering "+enemy1.Name);
+                Console.WriteLine(enemy1.Name+" attacking you ... ");
+                Console.WriteLine("Choose your action :");
+                Console.WriteLine("1. Single Attack");
+                Console.WriteLine("2. Swing Attack");
+                Console.WriteLine("3. Defend");
+                Console.WriteLine("4. Run Away");
+
+                while (!player.IsDead && !enemy1.IsDead && !player.IsRunningAway)
+                {
+                    string playerAction = Console.ReadLine();
+                    switch(playerAction){
+                        case "1" :
+                        Console.WriteLine(player.Name+" is doing Single Attack");
+                        enemy1.GetHit(player.AttackPower);
+                        player.Experience += 0.3f;
+                        enemy1.Attack(enemy1.AttackPower);
+                        player.GetHit(enemy1.AttackPower);
+                        Console.Write("Player Health : "+player.Health+" | Enemy Health : "+enemy1.Health+"\n");
+                        break;
+                        case "2" :
+                        player.Swing();
+                        player.Experience += 0.9f;
+                        enemy1.GetHit(player.AttackPower);
+                        Console.Write("Player Health : "+player.Health+" | Enemy Health : "+enemy1.Health+"\n");
+                        break;
+                        case "3" :
+                        player.Rest();
+                        Console.WriteLine("Energy is being restored ...");
+                        enemy1.Attack(enemy1.AttackPower);
+                        player.GetHit(enemy1.AttackPower);
+                        break;
+                        case "4" :
+                        Console.WriteLine(player.Name+" is running away...");
+                        player.RunningAway();
+                        break;
+                    }
+                }
+
+                Console.WriteLine(player.Name+" get "+player.Experience+" experience point.");
+            }else{
+                Console.WriteLine("Goodbye !");
+                Console.Read();
+            }
+        }
+    }
+
+    class Novice
+    {
+        public int Health { get; set; }
+        public string Name { get; set; }
+        public int AttackPower { get; set; }
+        public int SkillSlot { get; set; }
+        public bool IsDead { get; set; }
+        public float Experience { get; set; }
+        public bool IsRunningAway { get; set; }
+        Random rnd = new Random();
+
+        public Novice(){
+            Health = 100;
+            SkillSlot = 0;
+            AttackPower = 1;
+            IsDead = false;
+            Experience = 0f;
+            Name = "Newbie";
+        }
+
+        public void Swing(){
+            if(SkillSlot > 0){
+                Console.WriteLine("SWINGG!!!");
+                AttackPower = AttackPower + rnd.Next(3,11);
+                SkillSlot--;
+            }else{
+                Console.WriteLine("You do not have energy");
+            }
+        }
+
+        public void GetHit(int hitValue){
+            Console.WriteLine(Name+" get hit by "+hitValue);
+            Health = Health - hitValue;
+
+            if(Health <= 0){
+                Health = 0;
+                Die();
+            }
+        }
+
+        public void Rest(){
+            SkillSlot = 3;
+            AttackPower = 1;
+        }
+
+        public void Die(){
+            Console.WriteLine("You are dead, game over !");
+            IsDead = true;
+        }
+
+        public void RunningAway(){
+            IsRunningAway = true;
+        }
+    }
+
+    class Enemy
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }   
+        public bool IsDead { get; set; }
+        Random rnd = new Random();
+
+        public Enemy(string name){
+            Health = 50;
+            Name = name;
+        }
+
+        public void Attack(int damage){
+            AttackPower = rnd.Next(1,10);
+        }
+
+        public void GetHit(int hitValue){
+            Console.WriteLine(Name+" get hit by "+hitValue);
+            Health = Health - hitValue;
+
+            if(Health <= 0){
+                Health = 0;
+                Die();
+            }
+        }
+
+        public void Die(){
+            Console.WriteLine(Name+" is Dead");
+            IsDead = true;
+        }
+    }
+}
